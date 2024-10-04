c384cda9a29e48c5bf471783d23304d8c17f259c
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-10-04 12:24:50 +0200

    feat/37 CRUD user fix error with no return and returns type

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -22,35 +22,38 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/:id')
-  async findUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response): Promise<void> {
+  async findUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response): Promise<Response> {
     try {
       const user = await this.usersService.getUser(id);
-      res.status(200).json(user);
+      return res.status(200).json(user);
     } catch (error) {
       if (error instanceof NotFoundException) {
-        res.status(404).json({ message: Errors.USER_NOT_FOUND });
+        return res.status(404).json({ message: Errors.USER_NOT_FOUND });
       } else {
-        res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
+        return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
       }
     }
   }
 
   @Post()
-  async createUser(@Body(ValidationPipe) userDto: UserDto, @Res() res: Response): Promise<void> {
+  async createUser(
+    @Body(ValidationPipe) userDto: UserDto,
+    @Res() res: Response,
+  ): Promise<Response> {
     if (!validator.isEmail(userDto.email)) {
-      res.status(400).json({ message: Errors.INVALID_EMAIL });
+      return res.status(400).json({ message: Errors.INVALID_EMAIL });
     }
     // eslint-disable-next-line prettier/prettier
     const regexPassword = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
     if (!validator.matches(userDto.password, regexPassword)) {
-      res.status(400).json({ message: Errors.INVALID_PASSWORD });
+      return res.status(400).json({ message: Errors.INVALID_PASSWORD });
     }
     try {
       const user = await this.usersService.createUser(userDto);
-      res.status(201).json(user);
+      return res.status(201).json(user);
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
+      return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
     }
   }
 
@@ -59,36 +62,36 @@ export class UsersController {
     @Param('id', ParseIntPipe) id: number,
     @Body(ValidationPipe) userDto: UserDto,
     @Res() res: Response,
-  ): Promise<void> {
+  ): Promise<Response> {
     if (!validator.isEmail(userDto.email)) {
-      res.status(400).json({ message: Errors.INVALID_EMAIL });
+      return res.status(400).json({ message: Errors.INVALID_EMAIL });
     }
 
     if (!validator.isStrongPassword(userDto.password)) {
-      res.status(400).json({ message: Errors.INVALID_PASSWORD });
+      return res.status(400).json({ message: Errors.INVALID_PASSWORD });
     }
     try {
       const user = await this.usersService.updateUser(id, userDto);
-      res.status(200).json(user);
+      return res.status(200).json(user);
     } catch (error) {
       if (error instanceof NotFoundException) {
-        res.status(404).json({ message: Errors.USER_NOT_FOUND });
+        return res.status(404).json({ message: Errors.USER_NOT_FOUND });
       } else {
-        res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
+        return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
       }
     }
   }
 
   @Delete('/:id')
-  async deleteUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response): Promise<void> {
+  async deleteUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response): Promise<Response> {
     try {
       await this.usersService.deleteUser(id);
-      res.status(204).send('success');
+      return res.status(200).send('success');
     } catch (error) {
       if (error instanceof NotFoundException) {
-        res.status(404).json({ message: Errors.USER_NOT_FOUND });
+        return res.status(404).json({ message: Errors.USER_NOT_FOUND });
       } else {
-        res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
+        return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
       }
     }
   }
