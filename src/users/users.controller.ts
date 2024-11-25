c023c8495cc5db63e128a0be5f9b270faa4b0f8e
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-11-25 22:01:10 +0100

    fix/77 fixed errors handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,69 +7,38 @@ import {
   Delete,
   Put,
   ValidationPipe,
-  Res,
-  NotFoundException,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { UsersService } from './users.service';
 import { UserDto } from './user.dto';
-import { Errors } from '../Enums/enums';
-import validator from 'validator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/:id')
-  async findUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response): Promise<Response> {
-    try {
-      const user = await this.usersService.getUser(id);
-      return res.status(200).json(user);
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        return res.status(404).json({ message: Errors.USER_NOT_FOUND });
-      } else {
-        return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
-      }
-    }
+  @HttpCode(HttpStatus.OK)
+  async findUser(@Param('id', ParseIntPipe) id: number): Promise<UserDto> {
+    return await this.usersService.getUser(id);
   }
 
   @Put('/:id')
+  @HttpCode(HttpStatus.OK)
   async updateUser(
     @Param('id', ParseIntPipe) id: number,
     @Body(ValidationPipe) userDto: UserDto,
-    @Res() res: Response,
-  ): Promise<Response> {
-    if (!validator.isEmail(userDto.email)) {
-      return res.status(400).json({ message: Errors.INVALID_EMAIL });
-    }
-
-    if (!validator.isStrongPassword(userDto.password)) {
-      return res.status(400).json({ message: Errors.INVALID_PASSWORD });
-    }
-    try {
-      const user = await this.usersService.updateUser(id, userDto);
-      return res.status(200).json(user);
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        return res.status(404).json({ message: Errors.USER_NOT_FOUND });
-      } else {
-        return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
-      }
-    }
+  ): Promise<UserDto> {
+    return await this.usersService.updateUser(id, userDto);
   }
 
   @Delete('/:id')
-  async deleteUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response): Promise<Response> {
+  @HttpCode(HttpStatus.OK)
+  async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<void> {
     try {
       await this.usersService.deleteUser(id);
-      return res.status(200).send('success');
     } catch (error) {
-      if (error instanceof NotFoundException) {
-        return res.status(404).json({ message: Errors.USER_NOT_FOUND });
-      } else {
-        return res.status(500).json({ message: Errors.INTERNAL_SERVER_ERROR });
-      }
+      throw error; // Re-throw the error to ensure NestJS handles it and sends the correct status code
     }
   }
 }
