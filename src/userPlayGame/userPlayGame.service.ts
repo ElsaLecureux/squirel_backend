b3432933ba789b82080fe0fc082bc5866156b035
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-03-06 20:37:59 +0100

    feat/121 added update query and changed getUserGamesWon to get all games

--- src/userPlayGame/userPlayGame.service.ts
+++ src/userPlayGame/userPlayGame.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { DataSource } from 'typeorm';
-import { UserPlayGameDto } from './userPlayGame.dto';
+import { UserPlayGameDto, UserPlayGameFullDto } from './userPlayGame.dto';
 import { Errors } from 'src/Enums/enums';
 
 @Injectable()
@@ -11,21 +11,42 @@ export class UserPlayGameService {
     private readonly dataSource: DataSource,
   ) {}
 
-  async getUserGameWon(userPlayGameDto: UserPlayGameDto): Promise<any> {
+  async getUserGamesWon(userPlayGameDto: UserPlayGameDto): Promise<UserPlayGameFullDto> {
     const userId = userPlayGameDto.userId;
-    const gameId = userPlayGameDto.gameId;
-    let infos = [];
-    infos = await this.dataSource.query(
-      `SELECT * FROM userplaygame WHERE userId=$1 AND gameId=$2`,
-      [userId, gameId],
-    );
+    //ajouter join pour avoir le nom du jeu!
+    const infos = await this.dataSource.query(`SELECT * FROM userplaygame WHERE userId=$1`, [
+      userId,
+    ]);
     if (infos.length === 0) {
       throw new NotFoundException(Errors.DATA_NOT_FOUND);
     }
     return infos;
   }
 
-  async updateUserGameWon(userId: number, gameId: number): Promise<any> {
-    //return this.dataSource.query(`SELECT * FROM USERS`);
+  async playerWonGame(userPlayGameDto: UserPlayGameDto): Promise<UserPlayGameFullDto> {
+    const userId = userPlayGameDto.userId;
+    const gameId = userPlayGameDto.gameId;
+    try {
+      let infos = [];
+      infos = await this.dataSource.query(
+        `SELECT * FROM userplaygame WHERE userId=$1 AND gameId=$2`,
+        [userId, gameId],
+      );
+      if (infos.length === 0) {
+        const result = await this.dataSource.query(
+          `INSERT INTO userplaygame (userId, gameId, numberOfTimeWon) VALUES ($1, $2, 1) RETURNING *`,
+          [userId, gameId],
+        );
+        return result[0];
+      }
+      const result = await this.dataSource.query(
+        `UPDATE userplaygame SET numberOfTimeWon= numberOfTimeWon + 1  WHERE userId=$1 AND gameId=$2 RETURNING *`,
+        [userId, gameId],
+      );
+      return result[0];
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException(Errors.INTERNAL_SERVER_ERROR);
+    }
   }
 }
