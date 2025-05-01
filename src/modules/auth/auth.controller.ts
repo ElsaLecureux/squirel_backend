b9a1bcf90a463035f14729dfb1555b265cc799e1
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-01 08:52:18 +0200

    feat/131 added decorators to controllers

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signIn.dto';
 import { UsersService } from 'src/modules/users/users.service';
 import { CreateUserDto } from './dto/createUser.dto';
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -12,12 +13,22 @@ export class AuthController {
 
   @Post('signin')
   @HttpCode(HttpStatus.OK)
+  @ApiBody({
+    type: SignInDto,
+    description: 'Json structure for user sign in object',
+  })
+  @ApiResponse({ status: 200, description: 'The user has been successfully signed in' })
   async signIn(@Body(ValidationPipe) signInDto: SignInDto): Promise<{ access_token }> {
     return await this.authService.signIn(signInDto);
   }
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Json structure for user creation object',
+  })
+  @ApiResponse({ status: 201, description: 'The user has been successfully created' })
   async signUp(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<{ access_token }> {
     console.log(createUserDto);
     return await this.authService.signUp(createUserDto);
