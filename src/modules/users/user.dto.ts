cc0bda778e92d193da4a1c98afbc72363ebd526b
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-01 07:30:30 +0200

    feat/131 added swagger to project with DTO decorators

--- src/modules/users/user.dto.ts
+++ src/modules/users/user.dto.ts
@@ -1,19 +1,23 @@
 import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { Errors } from '../../shared/enums/errorsEnum';
+import { ApiProperty } from '@nestjs/swagger';
 
 // eslint-disable-next-line prettier/prettier
 const regexPassword = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 
 export class UserDto {
+  @ApiProperty()
   @IsString({ message: 'Username must be a string.' })
   @IsNotEmpty({ message: Errors.MISSING_REQUIRED_FIELD })
   username: string;
 
+  @ApiProperty()
   @IsString({ message: 'Email must be a string.' })
   @IsNotEmpty({ message: Errors.MISSING_REQUIRED_FIELD })
   @IsEmail({}, { message: Errors.INVALID_EMAIL })
   email: string;
 
+  @ApiProperty()
   @IsString({ message: 'Password must be a string.' })
   @IsNotEmpty({ message: Errors.MISSING_REQUIRED_FIELD })
   @Matches(regexPassword, {
@@ -22,5 +26,6 @@ export class UserDto {
   password: string;
 
   //@IsNumber()
+  @ApiProperty()
   id: number;
 }
