a2bc6a12c644dde270c1c9b5b4bd82013837f3fa
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-08 17:12:38 +0200

    feat/134 added docker compose, dockerfile and migration for database, added new envirnoments dev and prod

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const dataSource = app.get(DataSource);
+  await dataSource.runMigrations();
   const config = new DocumentBuilder()
     .setTitle('Squirel API')
     .setDescription(
