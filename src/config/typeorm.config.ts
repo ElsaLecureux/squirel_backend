54e034b419fbe19cc04f20e590551bf0c2243889
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-08 17:12:38 +0200

    feat/134 added docker compose, dockerfile and migration for database, added new envirnoments dev and prod

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, '..', 'migrations', '*.{ts,js}')],
+});
