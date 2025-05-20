e07961c047aa3e8eb3fd330025556e7fa1006982
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-20 11:46:14 +0200

    feat/123 added controller, service and updated module

--- src/schemas/gamePlay/gamePlay.module.ts
+++ src/schemas/gamePlay/gamePlay.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { GamePlayMemory, gamePlayMemorySchema } from './gamePlayMemory';
+import { GamePlayMemory, gamePlayMemorySchema } from './gamePlay.schema';
+import { GamePlayController } from './gamePlay.controller';
+import { GamePlayService } from './gamePlay.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: GamePlayMemory.name, schema: gamePlayMemorySchema }]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [GamePlayController],
+  providers: [GamePlayService],
 })
 export class GamePlayModule {}
